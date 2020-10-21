349daa120fbf6d834ad5d85f17f06781ee29a94b
Cristian Flores <al221910343@gmail.com>
2020-10-21 14:44:57 -0500

    Separando la logica en varios archivos

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,7 @@
+import 'package:examen/src/app.dart';
+// import 'src/app.dart';
 import 'package:flutter/material.dart';
 
 main() {
   runApp(new MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(context) {
-    return MaterialApp(
-        home: Center(
-      child: Text('Hola mundo'),
-    ));
-  }
 }
